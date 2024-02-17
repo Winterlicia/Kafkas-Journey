9fdf29ed5a7af68f00440859a4d5657d485c38f9
Kaden司徒逸希 <127336883+Winterlicia@users.noreply.github.com>
2024-02-16 23:18:18 -0500

    Add new bossmap, obtainable items on the map, animated boss battle, and lore
    TO-DO:
    Fix the exception causing bossmap.csv to malfunction in the 2nd loop in Main.java
    (Unsure why this is happening since it works fine in a separate testing program)

---
+++ Simple RPG/Simple RPG/Enemy.java
@@ -0,0 +1,56 @@
+import java.awt.image.BufferedImage;
+
+public class Enemy {
+    //Enemy's ATTRIBUTES: image and stats
+    BufferedImage imgEnemy;
+    int intHP = 40;
+    int intDMG = 10;
+    int intDEF = 10;
+
+    //Initialize the Object's (Enemy) constructor with its parameters:
+    public Enemy(BufferedImage imgEnemy, int intHP, int intDMG, int intDEF) {
+        this.imgEnemy = imgEnemy;
+        this.intHP = intHP;
+        this.intDMG = intDMG;
+        this.intDEF = intDEF;
+    }
+
+    //NOTE: G&S FOR DMG, DEF MAY NOT BE NEEDED, DEPENDS IF ITEMS TO INCREASE THOSE ARE ADDED
+    //Below are Getters and Setters of the object:
+    // 1) Setters allow us to continuously update the Enemy's attributes.
+    // 2) Getters allow us to take the Enemy's current attributes to control the battle engine.
+    public BufferedImage getEnemyImage() {
+        return this.imgEnemy;
+    }
+
+    public void setEnemyImage(BufferedImage imgEnemy) {
+        this.imgEnemy = imgEnemy;
+    }
+
+    //Important for healing in buildings and taking damage:
+    public int getCurrentHP() {
+        return this.intHP;
+    }
+
+    public void setNewHP(int intHP) {
+        this.intHP = intHP;
+    }
+
+    public int getCurrentDMG() {
+        return this.intDMG;
+    }
+
+    public void setNewDMG(int intDMG) {
+        this.intDMG = intDMG;
+    }
+
+    public int getCurrentDEF() {
+        return this.intDEF;
+    }
+
+    public void setNewDEF(int intDEF) {
+        this.intDEF = intDEF;
+    }
+
+}
+
